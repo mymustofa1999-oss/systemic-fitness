d74ec94c87e14a1a686d6b7bfd970f0145ccda72
mymustofa1999 <my.mustofa1999@gmail.com>
2026-09-08 18:50:19 +0700

    chore: remove test files

--- test_categories.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios');
-axios.post("http://localhost:8080/api/digital-library/movements", {
-  name: "testcase",
-  body_part: "lower",
-  categories: ["MC"],
-  target_gender: "universal"
-}).then(res => console.log(res.data)).catch(err => console.log(err.response.data));
